d14c047ddd30e79c5e879be8a0bbc2ef034f1cbb
Geoffrey <Geoffrey@128.4.92.136>
2017-02-20 12:30:02 -0500

    Initial Commit

---
+++ Lab1-2/Lab1_Maven/src/main/java/GCD/Lab1_Maven/App.java
@@ -0,0 +1,13 @@
+package GCD.Lab1_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
